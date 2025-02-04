d62fc9b1321547ee81597631a4adc84eb39db118
Ju4n3sD3v <juanherrera@unal.edu.co>
2025-02-04 13:54:54 -0500

    constructor de factura

--- src/gestorAplicacion/gestion/Factura.py
+++ src/gestorAplicacion/gestion/Factura.py
@@ -1,2 +1,23 @@
+from datetime import date
+import Cliente
+from produccion import Tienda, Transporte, Producto
+
 class Factura:
-    pass
+    _id = 0
+    _fecha = date(2000, 1, 1)
+    _tienda: Tienda = Tienda()
+    _cliente: Cliente = Cliente()
+    _transporte: Transporte = Transporte()
+    _listaProductos:list[Producto()]
+    _costoEnvio: float
+    _total: float
+    totalCreados: int
+    def __init__(self, tienda, cliente, transporte, listaP, costoEnvio, fecha) -> None:
+        self._id = self.totalCreados + 1
+        self._fecha = fecha        
+        self._tienda = tienda
+        self._cliente = cliente
+        self._transporte = transporte
+        self._listaProductos = listaP
+        self._costoEnvio = costoEnvio
+        self.totalCreados += 1 
